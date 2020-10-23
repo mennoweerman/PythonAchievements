d735f5525098b32d9352dd75302b14a88898d03b
menno weerman <menno.weerman@gmai.com>
2020-10-23 12:02:40 +0200

    Family Ties opdracht

---
+++ Family Ties.py
@@ -0,0 +1,11 @@
+naam = "Menno"
+leeftijd = 16
+woonplaats = "Monnickendam"
+kmnaarschool = 20
+favourietegame = "Pokemon"
+favourietefilm = "Jojo's Bizarre Adventure"
+hobby = "gamen, games ontwikkelen en werken"
+
+print('Mijn naam is ' + naam + ', ik ben ' + str(leeftijd) + ' jaar oud en ik woon in ' + woonplaats + ', ik woon ' + str(kmnaarschool) + ' km van mijn school vandaan')
+#print(f"Mijn naam is {naam}")
+print('Mijn favouriete game is ' + favourietegame + ' en mijn favouriete film is ' + favourietefilm + ', mijn hobbies zijn ' + hobby)
